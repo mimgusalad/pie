f15b74206bf19380dbd7ad0180a6ddec43478733
Hyeonbin <seohyunbin2@gmail.com>
2023-06-06 17:25:46 +0900

    map second commit

--- src/main/map/src/data/Data.js
+++ src/main/map/src/data/Data.js
@@ -154,8 +154,8 @@ export const Data = [
         lng: 128.75096690986445,
         title: "경북 경산시 대동 8",
         address: "(지번) 청운2로 13",
-        fee: "월세 300 / 33",
-        price: 33,
+        fee: "월세 300 / 40",
+        price: 40,
         houseType: "오픈형",
         reviews: 64,
         tags: [
@@ -281,9 +281,9 @@ export const Data = [
         lng: 128.757913529602,
         title: "경북 경산시 압량읍 청운1로 84",
         address: "(지번) 압량읍 부적리 386-3",
-        fee: "월세 300 / 33",
-        price: 33,
-        houseType: "오픈형",
+        fee: "월세 400 / 50",
+        price: 50,
+        houseType: "복층",
         reviews: 64,
         tags: [
             {
@@ -381,8 +381,8 @@ export const Data = [
         lng: 128.751833240042,
         title: "경북 경산시 둥지로 55",
         address: "(지번) 임당동 647-6",
-        fee: "월세 150 / 24",
-        price: 24,
+        fee: "월세 200 / 31",
+        price: 31,
         houseType: "분리형",
         reviews: 64,
         tags: [
@@ -475,5 +475,581 @@ export const Data = [
             },
         ],
         rating: 3.4,
+    },
+    // 새로운 데이터 tag들 손보기
+    {
+        lat: 35.8384081504613,
+        lng: 128.75723624076,
+        title: "경북 경산시 조영길 7-2",
+        address: "(지번) 조영동 346",
+        fee: "월세 200 / 30",
+        price: 30,
+        houseType: "분리형",
+        reviews: 64,
+        tags: [
+            {
+                label: "옵션 상태",
+                key: "options",
+            },
+            {
+                label: "일조량",
+                key: "light_insulation",
+            },
+            {
+                label: "냄새",
+                key: "smell",
+            },
+        ],
+        rating: 3.8,
+    },
+    {
+        lat: 35.8375504650421,
+        lng: 128.756422514833,
+        title: "경북 경산시 대학로 312-10",
+        address: "(지번) 조영동 323-1",
+        fee: "월세 300 / 33",
+        price: 33,
+        houseType: "오픈형",
+        reviews: 64,
+        tags: [
+            {
+                label: "일조량",
+                key: "light_insulation",
+            },
+            {
+                label: "소음",
+                key: "sound",
+            },
+            {
+                label: "냄새",
+                key: "smell",
+            },
+        ],
+        rating: 4.2,
+    },
+    {
+        lat: 35.8386989941918,
+        lng: 128.757590043484,
+        title: "경북 경산시 조영1길 10",
+        address: "(지번) 조영동 349-1",
+        fee: "월세 150 / 24",
+        price: 24,
+        houseType: "옥탑",
+        reviews: 64,
+        tags: [
+            {
+                label: "치안",
+                key: "security",
+            },
+            {
+                label: "벌레",
+                key: "bug",
+            },
+            {
+                label: "편의시설",
+                key: "convenience",
+            },
+        ],
+        rating: 3.1,
+    },
+    {
+        lat: 35.838752552344,
+        lng: 128.757994388738,
+        title: "경북 경산시 조영1길 14",
+        address: "(지번) 조영동 349-5",
+        fee: "월세 200 / 26",
+        price: 26,
+        houseType: "오픈형",
+        reviews: 64,
+        tags: [
+            {
+                label: "쓰레기 처리",
+                key: "garbage",
+            },
+            {
+                label: "편의시설",
+                key: "convenience",
+            },
+            {
+                label: "소음",
+                key: "sound",
+            },
+        ],
+        rating: 3.7,
+    },
+    {
+        lat: 35.8385219755679,
+        lng: 128.758639472469,
+        title: "경북 경산시 조영1길 16-5",
+        address: "(지번) 조영동 349-5",
+        fee: "월세 300 / 37",
+        price: 37,
+        houseType: "분리형",
+        reviews: 64,
+        tags: [
+            {
+                label: "벌레",
+                key: "bug",
+            },
+            {
+                label: "치안",
+                key: "security",
+            },
+            {
+                label: "일조량",
+                key: "light_insulation",
+            },
+        ],
+        rating: 4.6,
+    },
+    {
+        lat: 35.8384726220085,
+        lng: 128.759240746502,
+        title: "경북 경산시 조영길 29-2",
+        address: "(지번) 조영동 357-2",
+        fee: "월세 100 / 18",
+        price: 18,
+        houseType: "반지하",
+        reviews: 64,
+        tags: [
+            {
+                label: "냄새",
+                key: "smell",
+            },
+            {
+                label: "일조량",
+                key: "light_insulation",
+            },
+            {
+                label: "벌레",
+                key: "bug",
+            },
+        ],
+        rating: 2.4,
+    },
+    {
+        lat: 35.8388649897794,
+        lng: 128.759971632304,
+        title: "경북 경산시 압량읍 화랑로59길 29",
+        address: "(지번) 압량읍 부적리 525-30",
+        fee: "월세 100 / 18",
+        price: 19,
+        houseType: "반지하",
+        reviews: 64,
+        tags: [
+            {
+                label: "편의시설",
+                key: "convenience",
+            },
+            {
+                label: "쓰레기 처리",
+                key: "garbage",
+            },
+            {
+                label: "냄새",
+                key: "smell",
+            },
+        ],
+        rating: 2.8,
+    },
+    {
+        lat: 35.8430769842981,
+        lng: 128.756526015109,
+        title: "경북 경산시 압량읍 고분길 38",
+        address: "(지번) 압량읍 부적리 410-3",
+        fee: "월세 300 / 40",
+        price: 40,
+        houseType: "복층",
+        reviews: 64,
+        tags: [
+            {
+                label: "소음",
+                key: "sound",
+            },
+            {
+                label: "옵션 상태",
+                key: "options",
+            },
+            {
+                label: "치안",
+                key: "security",
+            },
+        ],
+        rating: 4.8,
+    },
+    {
+        lat: 35.8444006234629,
+        lng: 128.756909482421,
+        title: "경북 경산시 압량읍 청운2로17길 4-1",
+        address: "(지번) 압량읍 부적리 339-15",
+        fee: "월세 300 / 45",
+        price: 40,
+        houseType: "복층",
+        reviews: 64,
+        tags: [
+            {
+                label: "옵션 상태",
+                key: "options",
+            },
+            {
+                label: "일조량",
+                key: "light_insulation",
+            },
+            {
+                label: "편의시설",
+                key: "convenience",
+            },
+        ],
+        rating: 4.5,
+    },
+    {
+        lat: 35.8445701314675,
+        lng: 128.756890533339,
+        title: "경상북도 경산시 압량읍 청운2로17길 8-4",
+        address: "(지번) 경산시 압량읍 부적리 339-6",
+        fee: "월세 300 / 21",
+        price: 21,
+        houseType: "오픈형",
+        reviews: 64,
+        tags: [
+            {
+                label: "치안",
+                key: "security",
+            },
+            {
+                label: "벌레",
+                key: "bug",
+            },
+            {
+                label: "냄새",
+                key: "smell",
+            },
+        ],
+        rating: 3.5,
+    },
+    {
+        lat: 35.8447167767007,
+        lng: 128.757307255129,
+        title: "경북 경산시 압량읍 청운2로17길 4-9",
+        address: "(지번) 압량읍 부적리 339-11",
+        fee: "월세 200 / 25",
+        price: 25,
+        houseType: "옥탑",
+        reviews: 64,
+        tags: [
+            {
+                label: "일조량",
+                key: "light_insulation",
+            },
+            {
+                label: "벌레",
+                key: "bug",
+            },
+            {
+                label: "편의시설",
+                key: "convenience",
+            },
+        ],
+        rating: 3.1,
+    },
+    {
+        lat: 35.8447167767007,
+        lng: 128.757307255129,
+        title: "경북 경산시 압량읍 청운2로17길 4-9",
+        address: "(지번) 압량읍 부적리 339-11",
+        fee: "월세 200 / 25",
+        price: 25,
+        houseType: "옥탑",
+        reviews: 64,
+        tags: [
+            {
+                label: "벌레",
+                key: "bug",
+            },
+            {
+                label: "냄새",
+                key: "smell",
+            },
+            {
+                label: "소음",
+                key: "sound",
+            },
+        ],
+        rating: 3.1,
+    },
+    {
+        lat: 35.8431000555209,
+        lng: 128.752805550074,
+        title: "경북 경산시 청운2로11길 20",
+        address: "(지번) 임당동 649-9",
+        fee: "월세 300 / 30",
+        price: 30,
+        houseType: "오픈형",
+        reviews: 64,
+        tags: [
+            {
+                label: "냄새",
+                key: "smell",
+            },
+            {
+                label: "일조량",
+                key: "light_insulation",
+            },
+            {
+                label: "치안",
+                key: "security",
+            },
+        ],
+        rating: 3.8,
+    },
+    {
+        lat: 35.8437348102846,
+        lng: 128.753015304269,
+        title: "경북 경산시 둥지로10길 18-3",
+        address: "(지번) 조영동 557-4",
+        fee: "월세 300 / 33",
+        price: 33,
+        houseType: "지상층",
+        reviews: 64,
+        tags: [
+            {
+                label: "편의시설",
+                key: "convenience",
+            },
+            {
+                label: "일조량",
+                key: "light_insulation",
+            },
+            {
+                label: "옵션 상태",
+                key: "options",
+            },
+        ],
+        rating: 4.1,
+    },
+    {
+        lat: 35.8433535761848,
+        lng: 128.751771453455,
+        title: "경북 경산시 둥지로10길 27-4",
+        address: "(지번) 임당동 617",
+        fee: "월세 200 / 27",
+        price: 27,
+        houseType: "분리형",
+        reviews: 64,
+        tags: [
+            {
+                label: "소음",
+                key: "sound",
+            },
+            {
+                label: "치안",
+                key: "security",
+            },
+            {
+                label: "벌레",
+                key: "bug",
+            },
+        ],
+        rating: 3.9,
+    },
+    {
+        lat: 35.8433256535941,
+        lng: 128.751288519938,
+        title: "경북 경산시 청운2로11길 36",
+        address: "(지번) 임당동 605",
+        fee: "월세 100 / 17",
+        price: 17,
+        houseType: "반지하",
+        reviews: 64,
+        tags: [
+            {
+                label: "옵션 상태",
+                key: "options",
+            },
+            {
+                label: "일조량",
+                key: "light_insulation",
+            },
+            {
+                label: "쓰레기 처리",
+                key: "garbage",
+            },
+        ],
+        rating: 3.0,
+    },
+    {
+        lat: 35.8439000941965,
+        lng: 128.751451006057,
+        title: "경북 경산시 궁당로12길 14",
+        address: "(지번) 임당동 206-8",
+        fee: "월세 200 / 24",
+        price: 24,
+        houseType: "오픈형",
+        reviews: 64,
+        tags: [
+            {
+                label: "일조량",
+                key: "light_insulation",
+            },
+            {
+                label: "냄새",
+                key: "smell",
+            },
+            {
+                label: "치안",
+                key: "security",
+            },
+        ],
+        rating: 3.4,
+    },
+    {
+        lat: 35.8437681170534,
+        lng: 128.75065091092,
+        title: "경상북도 경산시 궁당로 54-4",
+        address: "(지번) 경산시 임당동 599-8",
+        fee: "월세 200 / 28",
+        price: 28,
+        houseType: "옥탑",
+        reviews: 64,
+        tags: [
+            {
+                label: "쓰레기 처리",
+                key: "garbage",
+            },
+            {
+                label: "벌레",
+                key: "bug",
+            },
+            {
+                label: "소음",
+                key: "sound",
+            },
+        ],
+        rating: 2.9,
+    },
+    {
+        lat: 35.8441741738335,
+        lng: 128.750396650114,
+        title: "경북 경산시 궁당로 56",
+        address: "(지번) 임당동 206-1",
+        fee: "월세 300 / 41",
+        price: 41,
+        houseType: "복층",
+        reviews: 64,
+        tags: [
+            {
+                label: "벌레",
+                key: "bug",
+            },
+            {
+                label: "일조량",
+                key: "light_insulation",
+            },
+            {
+                label: "편의시설",
+                key: "convenience",
+            },
+        ],
+        rating: 4.2,
+    },
+    {
+        lat: 35.8427709231761,
+        lng: 128.75130078079,
+        title: "경북 경산시 청운2로11길 35-5",
+        address: "(지번) 임당동 206-1",
+        fee: "월세 300 / 45",
+        price: 45,
+        houseType: "복층",
+        reviews: 64,
+        tags: [
+            {
+                label: "냄새",
+                key: "smell",
+            },
+            {
+                label: "소음",
+                key: "sound",
+            },
+            {
+                label: "옵션 상태",
+                key: "options",
+            },
+        ],
+        rating: 4.8,
+    },
+    {
+        lat: 35.8430511806584,
+        lng: 128.750329469776,
+        title: "경북 경산시 궁당로 44-5",
+        address: "(지번) 임당동 568-3",
+        fee: "월세 500 / 51",
+        price: 51,
+        houseType: "복층",
+        reviews: 64,
+        tags: [
+            {
+                label: "편의시설",
+                key: "convenience",
+            },
+            {
+                label: "쓰레기 처리",
+                key: "garbage",
+            },
+            {
+                label: "냄새",
+                key: "smell",
+            },
+        ],
+        rating: 4.8,
+    },
+    {
+        lat: 35.8434867411195,
+        lng: 128.749825179523,
+        title: "경북 경산시 궁당로 49",
+        address: "(지번) 임당동 563-8",
+        fee: "월세 300 / 32",
+        price: 32,
+        houseType: "오픈형",
+        reviews: 64,
+        tags: [
+            {
+                label: "소음",
+                key: "sound",
+            },
+            {
+                label: "벌레",
+                key: "bug",
+            },
+            {
+                label: "치안",
+                key: "security",
+            },
+        ],
+        rating: 3.8,
+    },
+    {
+        lat: 35.8431676024857,
+        lng: 128.748665162932,
+        title: "경북 경산시 둥지로 86",
+        address: "(지번) 임당동 571-9",
+        fee: "월세 300 / 40",
+        price: 40,
+        houseType: "복층",
+        reviews: 64,
+        tags: [
+            {
+                label: "소음",
+                key: "sound",
+            },
+            {
+                label: "일조량",
+                key: "light_insulation",
+            },
+            {
+                label: "냄새",
+                key: "smell",
+            },
+        ],
+        rating: 4.0,
     },
 ];
