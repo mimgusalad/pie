60d6a352bd5d4f65d38800d53739f7c81d5657d2
Jpep26 <jh97power@naver.com>
2023-06-09 15:46:43 +0900

    logout

--- src/main/java/com/itd5/homeReviewSite/signup/SecurityConfig.java
+++ src/main/java/com/itd5/homeReviewSite/signup/SecurityConfig.java
@@ -26,12 +26,12 @@ public class SecurityConfig {
                 .anyRequest().permitAll() //나머지 uri는 모든 접근 허용
                 .and()
                 .formLogin() // form login 관련 설정
-                .loginPage("/loginForm")
+                .loginPage("/account/login")
                 .usernameParameter("name") // Member가 username이라는 파라미터 갖고 있으면 안 적어도 됨.
                 .loginProcessingUrl("/login") // 로그인 요청 받는 url
-                .defaultSuccessUrl("/home") // 로그인 성공 후 이동할 url
+                .defaultSuccessUrl("/") // 로그인 성공 후 이동할 url
                 .and().oauth2Login()//oauth2 관련 설정
-                .loginPage("/loginForm") //로그인이 필요한데 로그인을 하지 않았다면 이동할 uri 설정
+                .loginPage("/") //로그인이 필요한데 로그인을 하지 않았다면 이동할 uri 설정
                 .defaultSuccessUrl("/") //OAuth 구글 로그인이 성공하면 이동할 uri 설정
                 .userInfoEndpoint()//로그인 완료 후 회원 정보 받기
                 .userService(oAuth2MemberService).and().and().build(); //
