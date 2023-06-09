400b93037e638a8e46802707b22367fba045ce84
kyungmin <dakjijisalad@gmail.com>
2023-06-09 13:43:02 +0900

    chat-app error fixed, css changed

---
+++ src/main/chat-app2/src/DesktopView.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import SBConversation from "@sendbird/uikit-react/Channel";
+import SBChannelList from "@sendbird/uikit-react/ChannelList";
+import SBChannelSettings from "@sendbird/uikit-react/ChannelSettings";
+
+
+function DesktopView() {
+  const [showSettings, setShowSettings] = useState(false);
+  const [currentChannelUrl, setCurrentChannelUrl] = useState("");
+  const conversationWrap = document.getElementsByClassName(
+    "sendbird-app__conversation-wrap"
+  )[0];
+
+  const renderSettingsBar = () => {
+    conversationWrap.style.marginRight = "318px";
+  };
+  const hideSettingsBar = () => {
+    conversationWrap.style.marginRight = "0px";
+  };
+
+  return (
+    <div className="sendbird-app__wrap">
+        <div className="sendbird-app__channellist-wrap">
+          <SBChannelList
+            onChannelSelect={(channel) => {
+              if (channel && channel.url) {
+                setCurrentChannelUrl(channel.url);
+              }
+            }}
+          />
+        </div>
+        <div className="sendbird-app__conversation-wrap">
+          <SBConversation
+            channelUrl={currentChannelUrl}
+            onChatHeaderActionClick={() => {
+              setShowSettings(true);
+              renderSettingsBar();
+            }}
+          />
+          {showSettings && (
+            <div className="sendbird-app__settingspanel-wrap">
+              <SBChannelSettings
+                channelUrl={currentChannelUrl}
+                onCloseClick={() => {
+                  setShowSettings(false);
+                  hideSettingsBar()
+                }}
+              />
+            </div>
+          )}
+        </div>
+    </div>
+  );
+}
+
+export default DesktopView;
