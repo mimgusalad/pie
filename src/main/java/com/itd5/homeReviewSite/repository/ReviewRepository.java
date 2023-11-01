05f030ebf325a88a60e59d430fb771a72adacce0
kyungmin <dakjijisalad@gmail.com>
2023-11-01 16:55:28 +0900

    feat: ai화면 검색기능

--- src/main/java/com/itd5/homeReviewSite/repository/ReviewRepository.java
+++ src/main/java/com/itd5/homeReviewSite/repository/ReviewRepository.java
@@ -11,4 +11,6 @@ public interface ReviewRepository extends JpaRepository<review_article, Long> {
     List<review_article> findByUserId(Long userId);
     List<review_article> findTop4ByOrderByArticleNo();
     List<review_article> findTop4ByAddressId(Long addressId);
+
+    List<review_article> getAllByAddressIdIn(List<Long> list);
 }
