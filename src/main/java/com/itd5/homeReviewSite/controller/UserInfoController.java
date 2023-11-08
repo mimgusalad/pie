719298e6c4f72e6996a18134091c0d13f6c5237d
kyungmin <dakjijisalad@gmail.com>
2023-11-08 15:24:20 +0900

    feat: 로그인한 사용자 정보 넘겨줌 - cors+쿠키 문제 해결 - localhost:8080/user-info로 get 요청하면 현재 로그인한 사용자 이름, 이메일, 닉네임 넘겨줌 - 프론트에서 사용자 정보 받아와서 브라우저 sessionStorage에 저장함

---
+++ src/main/java/com/itd5/homeReviewSite/controller/UserInfoController.java
@@ -0,0 +1,48 @@
+package com.itd5.homeReviewSite.controller;
+
+import com.itd5.homeReviewSite.model.UserInfo;
+import com.itd5.homeReviewSite.signup.PrincipalDetails;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableCaching
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+public class UserInfoController {
+    @GetMapping("/getCookie")
+    public String getCookie(HttpServletRequest request) {
+        HttpSession session = request.getSession(true);
+        String jsessionId = session.getId();
+        Cookie cookie = new Cookie("JSESSIONID", jsessionId);
+        return cookie.getValue();
+    }
+    @GetMapping("/user-info")
+    public UserInfo getUserInfo(){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserInfo user = null;
+        if (principal instanceof PrincipalDetails) {
+            PrincipalDetails principalDetails = (PrincipalDetails) principal;
+            String name = principalDetails.getMember().getName();
+            String email = principalDetails.getMember().getEmail();
+            String nickname = principalDetails.getMember().getNickname();
+            user = new UserInfo(name, email, nickname);
+            System.out.println(user.getName());
+        } else if (principal instanceof String) {
+            // Handle the case where principal is a String (e.g., username or identifier)
+            // You can log an error or take appropriate action.
+            user = new UserInfo("anonymous", "anonymous", "anonymous");
+        }
+        System.out.println("user name: " +user.getName());
+
+
+        return user;
+    }
+}
--- src/main/map/src/App.js
+++ src/main/map/src/App.js
@@ -1,67 +1,125 @@
 import "./style.css";
-import axios from "axios";
-import React, { useEffect } from "react";
-import NavUnderSide from "./NaviBar/NavUnderSide";
-import {
-  Routes,
-  Route,
-  Link,
-  useNavigate,
-  Outlet,
-  BrowserRouter,
-} from "react-router-dom";
+import React, {useEffect} from "react";
+import {Link, Route, Routes, useNavigate} from "react-router-dom";
 import RoomList from "./RoomList";
 import SuccDetail from "./succDetail";
 import ReviewDetail from "./ReviewDetail";
 import NavUnder from "./NaviBar/NavUnder";
 import MapPage2 from "./NaviBar/MapPage2";
 import Chat from "./Chat";
+import Temp from "./Temp";
+import Logo from "./img/siteLogo.png";
+import KakaoLogin from "./img/kakao_login_small.png";
+import axios from "axios";
+import {Cookies, useCookies} from "react-cookie";
 
-const mainMenus = [
-  {
-    label: "지도",
-    path: "/map",
-  },
-  {
-    label: "AI 추천",
-    path: "/searchRoom",
-  },
-  {
-    label: "승계방",
-    path: "/successionBoard",
-  },
-  {
-    label: "혜택",
-    path: "/benefits",
-  },
-];
+const cookies = new Cookies();
+axios.defaults.withCredentials = true;
+
+const getUserInfo = async () => {
+
+    return await axios.get('http://localhost:8080/user-info')
+        .then((res) => {
+            console.log('user data:', res.data);
+            const userData = res.data;
+            if(userData.name !== "anonymous") {
+                sessionStorage.setItem('user', JSON.stringify(res.data));
+            }
+            return res.data;
+        })
+        .catch((err) => {
+            console.log('user data err:', err);
+        });
+}
+
+function getCookie(){
+    return axios.get("http://localhost:8080/getCookie")
+        .then((res)=>{
+            return res.data;
+        }).catch((err)=>{
+            console.log('cookie get error: ', err);
+        });
+
+}
+
+function Button() {
+    const navigate = useNavigate();
+
+    const handleKakaoLogin = async () => {
+        try {
+            const userInfo = await getUserInfo();
+        } catch (error) {
+            // Handle the error (e.g., show an error message)
+        }
+    }
+
+    return (
+        <Link to="http://localhost:8080/oauth2/authorization/kakao"
+              className="user_profile__login"
+              onClick={handleKakaoLogin}>
+            <img src={KakaoLogin} alt="카카오 로그인" />
+        </Link>
+    );
+}
+
+function User(props){
+    const navigate = useNavigate();
+    return(
+        <div>
+            <a onClick={()=> navigate('/chat')} className="fa-brands fa-twitter">메시지</a>
+            <Link to="http://localhost:8080/account/myPage" className="fa-brands fa-facebook">{props.user.name}</Link>
+        </div>
+    )
+}
 export default function TempPage() {
-  return (
+  let navigate = useNavigate();
+  let JSESSIONID = "";
+  const [user, setUser] = React.useState();
+
+    useEffect(() => {
+        const userFromSessionStorage = sessionStorage.getItem('user');
+        if (userFromSessionStorage) {
+            setUser(JSON.parse(userFromSessionStorage));
+        } else {
+            getCookie()
+                .then((JSESSIONID) => {
+                    cookies.set('JSESSIONID', JSESSIONID, { path: '/' });
+                    return getUserInfo();
+                })
+                .then((userData) => {
+                    setUser(userData);
+                })
+                .catch((error) => {
+                    console.log('Error:', error);
+                });
+        }
+    }, []);
+
+    return (
       <div>
         <nav className="navigation">
           <div className="navigation__navitems">
-            <h1 className="navitem__logo">못난집</h1>
+            <Link to="http://localhost:8080/" className="navitem__logo">
+              <img alt="못난집" src={Logo}/>
+            </Link>
             <div className="navitem__main-menus">
-              {mainMenus.map((menu, index) => {
-                return (
-                    <a className="main-menus__item" href={menu.path}>
-                      <div key={index}>{menu.label}</div>
-                    </a>
-                );
-              })}
+              <a onClick={()=> navigate('/map')} className="main-menus__item">지도</a>
+              <Link to="http://localhost:8080/ai/list" className="main-menus__item">AI추천</Link>
+              <a onClick={()=> navigate('/successionBoard')} className="main-menus__item">승계방</a>
+              <Link to="http://localhost:8080/benefit/list" className="main-menus__item">혜택</Link>
             </div>
             <div className="navitem__user_menus">
-              <a className="user_menus__item user_msg" href="/chat">
-                <span>메시지</span>
-              </a>
               <div className="user_menus__item user_profile">
-                <div className="user_profile__user_name">넨도님</div>
+                <div className="user_profile__user_name">
+                    {user === null || user === undefined || user.name === "anonymous"? <Button /> : <User user = {user}/>}
+                </div>
               </div>
             </div>
           </div>
         </nav>
-<BrowserRouter>
     <Routes>
+      <Route path="/" element={<div>home</div>}/>
+      <Route path="/home" element={<div>home</div>}/>
       <Route path="/map" element={<NavUnder />} />
       <Route path="/detail/:roomId" element={<MapPage2 />} />
       <Route path="/succDetail/:succId" element={<SuccDetail />} />
@@ -71,8 +129,10 @@ export default function TempPage() {
         element={<ReviewDetail />}
       />
       <Route path="/chat" element={<Chat/>}/>
+
+      /* 리액트-스프링부트 연결 확인용*/
+      <Route path="/chacha" element={<Temp/>}/>
      </Routes>
-  </BrowserRouter>
       </div>
   )
 }
