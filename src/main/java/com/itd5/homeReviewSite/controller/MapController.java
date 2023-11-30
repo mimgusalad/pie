7ba5cf51e4c156cc1f18d9112d363fe7e3b3592b
kyungmin <dakjijisalad@gmail.com>
2023-11-30 17:40:06 +0900

    메인화면 api

--- src/main/java/com/itd5/homeReviewSite/controller/MapController.java
+++ src/main/java/com/itd5/homeReviewSite/controller/MapController.java
@@ -3,9 +3,11 @@ package com.itd5.homeReviewSite.controller;
 import com.itd5.homeReviewSite.Service.MapService;
 import com.itd5.homeReviewSite.model.Address;
 import com.itd5.homeReviewSite.model.Map;
+import com.itd5.homeReviewSite.model.Search;
 import com.itd5.homeReviewSite.model.review_article;
 import com.itd5.homeReviewSite.repository.AddressRepository;
 import com.itd5.homeReviewSite.repository.ReviewRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -66,37 +68,25 @@ public class MapController {
         return reviewRepository.findByAddressId(addressId);
     }
 
-    @GetMapping("match")
-    public String match(@RequestParam(value = "addressKeyword", required = false) String addressKeyword,
-                        @RequestParam(value = "firstKeyword", required = false) String firstKeyword,
-                        @RequestParam(value = "secondKeyword", required = false) String secondKeyword,
-                        @RequestParam(value = "thirdKeyword", required = false) String thirdKeyword,
-                        Model model) {
-
-        //전처리
-        /* 순차적으로 키워드가 입력되지 않은 경우
-           mainPage에서 alert 메시지를 출력하고 map 화면으로 전환되지 않음
-
-           주소 키워드가 입력되지 않은 경우에
-           inputAddressKeyword 는 사용자 지정으로
-           (비로그인 : 서버에서 알아서 지정)
-           (로그인 : 회원에 저장된 관심 지역 중 하나)
-         */
-
-        if (addressKeyword != null) {
-            model.addAttribute("addressKeyword", addressKeyword);
-        }
-        if (firstKeyword != null) {
-            model.addAttribute("firstKeyword", firstKeyword);
-        }
-        if (secondKeyword != null) {
-            model.addAttribute("secondKeyword", secondKeyword);
-        }
-        if (thirdKeyword != null) {
-            model.addAttribute("thirdKeyword", thirdKeyword);
-        }
-
-        return "map/list";
+    @ResponseBody
+    @PostMapping("match")
+    public void match(HttpServletResponse res, @RequestParam Search search) throws IOException {
+        res.sendRedirect("/map/searchQuery?addressKeyword="+search.getAddressKeyword()
+                +"&firstKeyword="+search.getFirstKeyword()
+                +"&secondKeyword="+search.getSecondKeyword()
+                +"&thirdKeyword="+search.getThirdKeyword());
     }
+
+    @ResponseBody
+    @GetMapping("searchQuery")
+    public List<Map> searchQuery(@RequestParam(value = "addressKeyword", required = false) String addressKeyword,
+                                 @RequestParam(value = "firstKeyword", required = false) String firstKeyword,
+                                 @RequestParam(value = "secondKeyword", required = false) String secondKeyword,
+                                 @RequestParam(value = "thirdKeyword", required = false) String thirdKeyword,
+                                 Model model) {
+        return mapService.getSearchQuery(addressKeyword, firstKeyword, secondKeyword, thirdKeyword);
+    }
+
+
 
 }
---
+++ src/main/java/com/itd5/homeReviewSite/model/Search.java
@@ -0,0 +1,11 @@
+package com.itd5.homeReviewSite.model;
+
+import lombok.Data;
+
+@Data
+public class Search {
+    String addressKeyword;
+    String firstKeyword;
+    String secondKeyword;
+    String thirdKeyword;
+}
