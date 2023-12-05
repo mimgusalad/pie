7582f66dd3a6b806f1d4020e8b89f47ad1042c4d
Jpep26 <jh97power@naver.com>
2023-12-05 16:20:18 +0900

    로그아웃,리뷰수정

--- src/main/map/src/MyPage.js
+++ src/main/map/src/MyPage.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './MyPage.css';
 
 function MyPage() {
   const [myReviewList, setMyReviewList] = useState([]);
   const [noReview, setNoReview] = useState(false);
   const [noSuccession, setNoSuccession] = useState(false);
   const [isLoggedOut, setIsLoggedOut] = useState(false);
+  const [logoutMessage, setLogoutMessage] = useState('');
   const navigate = useNavigate();
 
    // '내가 쓴 리뷰' 링크 클릭 핸들러
@@ -32,15 +34,30 @@ function MyPage() {
   const handleLogout = () => {
       axios.post('http://localhost:8080/logout')
         .then(response => {
-          setIsLoggedOut(true); // 로그아웃 상태를 true로 설정
-          setTimeout(() => {
-            window.location.href = '/';
-          }, 2000); // 2초 후에 로그인 페이지로 리다이렉트
+           alert('로그아웃이 정상적으로 완료되었습니다.'); // 로그아웃 성공 메시지 표시
+           window.location.href = '/'; // 메인 페이지로 리다이렉트
         })
         .catch(error => {
           console.log(error);
         });
     };
+
+    const confirmLogout = () => {
+        setLogoutMessage(''); // 메시지 초기화
+        navigate('/'); // 메인 화면으로 이동
+      };
+
+      const renderLogoutAlert = () => {
+          if (logoutMessage) {
+            return (
+              <div className="logout-alert">
+                <p>{logoutMessage}</p>
+                <button onClick={confirmLogout}>확인</button>
+              </div>
+            );
+          }
+          return null;
+        };
 
   return (
     <div className="container mt-4">
